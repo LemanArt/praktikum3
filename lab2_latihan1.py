da3c0b134a7d20c3f5829199dab30bf9474a5762
LemanArt <leman210101@gmail.com>
2021-11-17 02:37:31 +0700

    tambah file python lab2 latihan1&2

---
+++ lab2_latihan1.py
@@ -0,0 +1,7 @@
+A=int(input("Masukkan nilai A : "))
+B=int(input("Masukkan nilai B : "))
+
+if A > B :
+    print("Nilai terbesar dari dua buah bilangan ", A)
+else:
+    print("Nilai terbesar dari dua buah bilangan", B)
---
+++ lab2_latihan2.py
@@ -0,0 +1,6 @@
+A=int(input("bilangan ke-1 = "))
+B=int(input("bilangan ke-2 ="))
+C=int(input("bilangan ke-3 ="))
+
+urut = A,B,C
+print("Urutan bilangan :", sorted(urut))
